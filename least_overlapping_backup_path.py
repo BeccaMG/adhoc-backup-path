7fc00c86dc21b89e25f3b3dfcde724710b652ae4
Mostafa Fateen <mostafafateen@gmail.com>
2016-02-13 02:37:44 +0100

    The least overlapping backup path calculated and works fine

--- least_overlapping_backup_path.py
+++ least_overlapping_backup_path.py
@@ -3,8 +3,6 @@
 # RIP algorithm with backup path determined by the SECOND BEST COST
 # #################################################################
 
-# IMPORTANT, DOES NOT DETECT LOOPS
-
 import network_graph as graph_gen
 import networkx as nx
 import numpy as np
@@ -28,7 +26,7 @@ def generate_rip_graph(draw_flag, size):
 
     for i in range(0, n):
         # #################################################################
-        # The primary path
+        # ##################     The primary path       ###################
         # #################################################################
         new_distance_matrix = np.empty((n, n))
         new_distance_matrix.fill(sys.maxint)
@@ -49,21 +47,20 @@ def generate_rip_graph(draw_flag, size):
         n_graph.node[i]['buffer_queue'] = Queue.Queue(maxsize=n)
 
         # #################################################################
-        # The backup path
+        # ###################       The backup path     ###################
         # #################################################################
 
         new_path_matrix = [[]]  # A 2d array which will contain all possible paths to all nodes of the graph
 
         new_default_path = []
+        new_backup_path = []
         for k in range(0, n):
             new_default_path.append('N/A')
-
-        new_backup_path = np.empty(n)
-        new_backup_path.fill(None)
+            new_backup_path.append('N/A')
 
         n_graph.node[i]['path_matrix'] = new_path_matrix
         n_graph.node[i]['primary_path'] = new_default_path
-        n_graph.node[i]['backup_path_index'] = new_backup_path
+        n_graph.node[i]['backup_path'] = new_backup_path
         n_graph.node[i]['path_buffer_queue'] = Queue.Queue()
 
     n_graph.nodes(data=True)
@@ -86,7 +83,6 @@ def rip_first_iteration(G):
         bwv[n] = 0
         pp[n] = []
         edges_from_n = G[n].items()  # Obtain all edges from node n
-        # print "I am in node %d" % n
         for e, eattr in edges_from_n:  # For each edge-to-node e
             w = eattr['weight']
             dm[e, e] = w
@@ -94,11 +90,6 @@ def rip_first_iteration(G):
             nhv[e] = e
             pm.append([e])
             pp[e] = [e]
-            # print "My weight to %d is %d" % (e, w)
-            # print "My matrix"
-            # print dm
-            # print "My vector"
-            # print bwv
 
 
 # Pass the best distances vectors (or best weights vectors to all the neighbors)
@@ -123,7 +114,6 @@ def path_enqueue(G, n1, n0, path):
     path_to_enqueue = [n0]
     path_to_enqueue.extend(path)
     pbq.put(path_to_enqueue)
-    # print "Node %d: Pair: %s" % (n1, pair_to_enqueue)
 
 
 # Enqueue on node to receive updates with redundancy
@@ -138,7 +128,6 @@ def node_enqueue(G, n1, n0, vector, path):
             path_to_enq.append(p)
     to_enqueue = n0, vector, path_to_enq
     bq.put(to_enqueue)
-    # print "Node %d: Pair: %s" % (n1, pair_to_enqueue)
 
 
 # Computes all the new distances after broadcast
@@ -151,7 +140,6 @@ def rip_update_distance_matrix(G):
 
         pm = nattr['path_matrix']
         pp = nattr['primary_path']
-        bpi = nattr['backup_path_index']
         pbq = nattr['path_buffer_queue']
 
         while not bq.empty():
@@ -160,14 +148,12 @@ def rip_update_distance_matrix(G):
             received_vector = element[1]  # Received best distance vector from my neighbor
             received_path = element[2]
             distance_to_neighbor = dm[origin_node, origin_node]
-            # print "Me envian %s desde %d con distancia %d" % (received_vector, origin_node, distance_to_neighbor)
             for i in range(0, len(bwv)):
                 # Update the distance to the node via the neighbor
                 if i != n:  # If it's not myself
                     old_distance = dm[i, origin_node]
                     new_distance = received_vector[i] + distance_to_neighbor
                     new_path = received_path[i]
-                    # print "i: %d, n: %d, distance: %d" % (i, n, received_vector[i])
                     if new_distance < old_distance:
                         dm[i, origin_node] = new_distance
                         global convergence
@@ -188,25 +174,64 @@ def rip_update_distance_matrix(G):
                     for j in range(0, len(pm[i])):
                         if pm[i][j] != path[j]:
                             break
-                        if j == len(pm[i])-1:
+                        if j == len(pm[i]) - 1:
                             pm[i] = path
                             appended = True
             if not (appended or path in pm):
                 pm.append(path)
 
-        print "I am in node %d" % n
-        print "My matrix"
-        print dm
-        print "My vector"
-        print bwv
-        print "\n"
+
+def make_unique(original_list):
+    unique_list = []
+    [unique_list.append(obj) for obj in original_list if obj not in unique_list]
+    return unique_list
 
 
-# def calculate_backup(G):
-#     for n, nattr in G.nodes(data=True):
-#         pm = nattr['path_matrix']
-#         pp = nattr['primary_path']
-#         bpi = nattr['backup_path_index']
+def get_pairs(array):
+    pairs = []
+    for i in range(0, len(array)-1):
+        pairs.append([array[i], array[i+1]])
+    return pairs
+
+
+def calculate_backup(G):
+    for n, nattr in G.nodes(data=True):
+        pm = nattr['path_matrix']
+        pp = nattr['primary_path']
+        bp = nattr['backup_path']
+
+        pm = make_unique(pm)
+
+        overlap_count = np.empty(len(pm))
+
+        for i in range(0, len(pp)):
+            overlap_count.fill(-1)
+            if n == i:
+                bp[i] = []
+            else:
+                cpp = get_pairs(pp[i])
+                for j in range(0, len(pm)):
+                    if i in pm[j]:
+                        overlap_count[j] = 0
+                        cpm = pm[j][:pm[j].index(i)+1]
+                        cpm = get_pairs(cpm)
+                        if pp[i][0] == pm[j][0]:
+                            overlap_count[j] += 1
+                        for k in cpp:
+                            if k in cpm:
+                                overlap_count[j] += 1
+                temp = -1
+                for j in range(0, len(overlap_count)):
+                    if overlap_count[j] != -1:
+                        if temp == -1:
+                            temp = j
+                        elif overlap_count[j] < overlap_count[temp]:
+                            temp = j
+
+                print "pm = %s\ni=%d\noc=%s\ntemp=%d" % (pm, i, overlap_count, temp)
+                bp[i] = pm[temp][:pm[temp].index(i)+1]
+        nattr['backup_path'] = bp
+        nattr['path_matrix'] = pm
 
 
 if __name__ == '__main__':
@@ -219,19 +244,17 @@ if __name__ == '__main__':
         rip_broadcast(network_graph)
         rip_update_distance_matrix(network_graph)
 
+    # calculate_backup(network_graph)
+
     # Comparing with bellman-ford for testing
     for n, nattr in network_graph.nodes(data=True):  # For each node n and attribute nattr
-        # print "Our RIP:"
-        # print "(%d,%s)" % (n, nattr['best_weights_vector'])
-        # print "(%d,%s)" % (n, nattr['default_next_hop'])
+
+        calculate_backup(network_graph)
+
         print "Node %d\n==============" % n
         for m in nattr['primary_path']:
-                print "path = %s" % m
-
-        # pred, dist = nx.bellman_ford(network_graph, n)
-        # print "Bellman_ford:"
-        # print sorted(dist.items())
-        # print sorted(pred.items())
-        # break
+            print "path = %s" % m
+        # print "\nBackup = %s" % nattr['backup_path']
+        print "\nMatrix =\n%s" % nattr['path_matrix']
 
     graph_gen.draw_graph(network_graph)
